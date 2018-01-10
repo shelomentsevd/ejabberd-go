92a23c9f209877e998c9f1f420646d797221ac5d
Shelomentsev Dmitriy <dshelomentsev@life-team.net>
2018-01-10 17:33:37 +0300

    Example

---
+++ examples/dummy.go
@@ -0,0 +1,36 @@
+package main
+
+import "github.com/shelomentsevd/ejabberd-go"
+
+type Dummy struct {
+}
+
+func (dummy Dummy) Auth(user, server, password string) bool {
+	return true
+}
+
+func (dummy Dummy) IsUser(user, server string) bool {
+	return true
+}
+
+func (dummy Dummy) SetPassword(user, server, password string) bool {
+	return false
+}
+
+func (dummy Dummy) TryRegister(user, server, password string) bool {
+	return false
+}
+
+func (dummy Dummy) RemoveUser(user, server string) bool {
+	return false
+}
+
+func (dummy Dummy) RemoveUser3(user, server, password string) bool {
+	return false
+}
+
+func main() {
+	external := jabberd.NewExternal(dummy)
+
+	external.start()
+}
